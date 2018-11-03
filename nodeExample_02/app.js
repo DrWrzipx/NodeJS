7348b3ac236cacb71589d01e2ab7e60c60328500
Dr_Matic <matic.drobez@gmail.com>
2018-11-03 10:43:20 +0100

    Fourth commit.

--- nodeExample_02/app.js
+++ nodeExample_02/app.js
@@ -3,13 +3,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
+const shopData = require('./routes/shop');
 
 app.use(bodyParser.urlencoded());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+app.use('/admin', adminData.routes);
+app.use(shopData.routes);
 
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
